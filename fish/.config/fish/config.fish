babcf1aa90286d83924bd617479e50a59f680008
Polka37 <209836559+Polka37@users.noreply.github.com>
2025-12-21 01:50:26 +0100

    add git aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,10 +5,20 @@ end
 
 set -U fish_greeting
 
+#Aliases
 alias tree='tree -C'
 alias logout='cinnamon-session-quit --logout --no-prompt'
 alias vim='nvim'
+alias yt-dlp='~/Documents/Appimage/yt-dlp_linux -t mp3 --embed-thumbnail --embed-metadata --embed-subs --paths ~/Music/'
 alias bios='systemctl reboot --firmware-setup'
+
+#Git aliases
+alias gits='git status'
+alias gitam='git commit -am'
+alias gitpl='git pull'
+alias gitpu='git push'
+alias gitl='git log'
+alias gitr='git restore'
 
 set -U EDITOR nvim
 
