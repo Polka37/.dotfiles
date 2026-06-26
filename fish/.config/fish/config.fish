a5e039d2a45a0da239c0e0bfd8cf1b1b62941c25
Polka37 <209836559+Polka37@users.noreply.github.com>
2026-06-26 17:10:22 +0200

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,7 +9,7 @@ set -U fish_greeting
 alias tree='tree -C'
 alias logout='cinnamon-session-quit --logout --no-prompt'
 alias vim='nvim'
-alias yt-dlp='~/Documents/Appimage/yt-dlp_linux -t mp3 --embed-thumbnail --embed-metadata --embed-subs --embed-chapters --paths ~/Music/'
+alias yt-dlp='~/Documents/Appimage/yt-dlp_linux -t mp4 --embed-thumbnail --embed-metadata --embed-subs --embed-chapters --paths ~/Music/'
 alias bios='systemctl reboot --firmware-setup'
 alias cat='batcat' #Fancier cat command
 
